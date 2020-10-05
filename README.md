b393a1206e313ee1c30fc624d5eef3c1f128adc8
Shereen Elsayed <shereenhussein92@gmail.com>
2020-10-05 21:23:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Demo project for github actions
+
+try CS148 and do some cool stuff
---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import find_packages, setup
+
+# read in README
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, 'README.md'), 'rb') as f:
+    long_description = f.read().decode().strip()
+
+# requirements
+install_requires = ["click"]
+
+setup(
+    name = 'Hello-CS148',
+    description = 'Demo application for github actions',
+    author = 'Shereen Elsayed',
+    author_email = 's_elsayed@ucsb.edu',
+
+    packages = find_packages(),
+
+    entry_points = {
+        'console_scripts': [
+            'CS148 = main:main',
+        ],
+    },
+
+    python_requires = '>=3.6.*',
+    install_requires = install_requires,
+    setup_requires = ['setuptools_scm'],
+    zip_safe=False,
+    use_scm_version = {
+        'write_to': '_version.py'
+    },
+
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+    ],
+
+)
